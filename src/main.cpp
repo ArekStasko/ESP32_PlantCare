3924be06893d72aa58e74a0dea366d52d6557520
Arkadiusz Staśko <arek.stasko@gmail.com>
2023-05-19 07:38:21 +0200

    Add wsClient

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,35 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <WebSocketsClient.h>
 
 #define WIFI_SSID "Orange_Swiatlowod_D5A0"
 #define WIFI_PASSWORD "FYPWSCW59ZAY"
+#define WS_HOST ""
+#define WS_PORT 443
+#define WS_URL ""
+
+WebSocketsClient wsClient;
+
+void handleMessage(uint8_t * payload){
+  
+}
+
+void onWSEvent(WStype_t type, uint8_t * payload, size_t length){
+  switch(type){
+    case WStype_CONNECTED: {
+      Serial.println("WS Connected");
+      break;
+    }
+    case WStype_DISCONNECTED : {
+      Serial.println("WS Disconnected");
+    }
+    case WStype_TEXT: {
+      Serial.printf("WS Message: %s\n", payload);
+      handleMessage(payload);
+      break;
+    }
+  }
+}
 
 void setup() {
   Serial.begin(921600);
@@ -11,6 +38,8 @@ void setup() {
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
   Serial.println("Connected");
+  wsClient.beginSSL(WS_HOST, WS_PORT, WS_URL, "", "wss");
+  wsClient.onEvent(onWSEvent);
 }
 
 bool isConnected = false;
@@ -30,4 +59,5 @@ void loop() {
     isConnected = false;
   }
 
+  wsClient.loop();
 }
