2940314fb735c31f37af44d915b4761cf7cc1c71
Arkadiusz Staśko <arek.stasko@gmail.com>
2023-05-28 20:16:34 +0200

    Add  command functionalities

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <ArduinoJson.h>
 #include <Arduino.h>
 #include <WiFi.h>
 #include <WebSocketsClient.h>
@@ -8,10 +9,89 @@
 #define WS_PORT 443
 #define WS_URL ""
 
+#define JSON_DOC_SIZE 2048
+#define MSG_SIZE 128
+
 WebSocketsClient wsClient;
 
+void sendErrorMessage(const char *error){
+  char msg[MSG_SIZE];
+
+  sprintf(msg, "{\"action\":\"msg\",\"body\":{\"type\":\"error\",\"error\":\"%s\"}}", error);
+
+  wsClient.sendTXT(msg);
+}
+
+uint8_t toMode(const char *val){
+  if(strcmp(val, "output") == 0){
+    return OUTPUT;
+  }
+
+  if(strcmp(val, "input_pullup") == 0){
+    return OUTPUT;
+  }
+
+  return INPUT
+}
+
+void sendOkMessage(){
+  char msg[MSG_SIZE];
+
+  sprintf(msg, "{\"action\":\"msg\",\"body\":{\"type\":\"status\",\"body\":\"ok\"}}");
+
+  wsClient.sendTXT(msg);
+}
+
 void handleMessage(uint8_t * payload){
-  
+   StaticJsonDocument<JSON_DOC_SIZE> doc;
+
+    DeserializationError error = deserializeJson(doc, payload);
+
+  // Test if parsing succeeds.
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+
+    sendErrorMessage(error.c_str());
+    return;
+  }
+
+  if(doc["type"].is<const char *>()){
+    sendErrorMessage("Invalid message type");
+    return;
+  }
+
+  if(strcmp(doc["type"], "cmd") == 0){
+    if(doc["body"].is<JsonObject>()){
+      sendErrorMessage("Invalid command body");
+      return;
+    }
+    
+    if(strcmp(doc["body"]["type"], "pinMode") == 0){
+      pinMode(doc["body"]["pin"], toMode(doc["body"]["mode"]));
+      sendOkMessage()
+      return;
+    }
+
+    if(strcmp(doc["body"]["type"], "digitalWrite") == 0){
+      digitalWrite(doc["body"]["pin"], doc["body"]["value"]);
+      sendOkMessage()
+      return;
+    }
+
+    if(strcmp(doc["body"]["type"], "digitalRead") == 0){
+      auto value = digitalRead(doc["body"]["pin"]);
+
+      char msg[MSG_SIZE];
+      sprintf(msg, "{\"action\":\"msg\",\"body\":{\"type\":\"output\",\"body\":%d}", value);
+      wsClient.sendTXT(msg);
+      
+      return;
+    }
+
+    sendErrorMessage("unsupported command");
+    return;
+  }
 }
 
 void onWSEvent(WStype_t type, uint8_t * payload, size_t length){
